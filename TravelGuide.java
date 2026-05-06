e990d2575b8943b03714bbe636d23e3e5767559a
pradhansanvi8 <pradhansanvi5@gmail.com>
2026-05-07 01:42:52 +0530

    Update TravelGuide.java

--- TravelGuide.java
+++ TravelGuide.java
@@ -1,32 +1,23 @@
-#include <iostream>
-using namespace std;
-
-class AbstractProduct {
-protected:
+abstract class P{
     int id;
-public:
-    virtual void get() = 0;
-    virtual void show() = 0;
-};
+    abstract void g();
+    abstract void s();
+}
 
-class Book : public AbstractProduct {
-public:
-    void get() { cin >> id; }
-    void show() { cout << "TRAVEL_GUIDE Book ID: " << id << endl; }
-};
+class B extends P{
+    void g(){ id=new java.util.Scanner(System.in).nextInt(); }
+    void s(){ System.out.println("Book "+id); }
+}
 
-class CD : public AbstractProduct {
-public:
-    void get() { cin >> id; }
-    void show() { cout << "CD ID: " << id << endl; }
-};
+class C extends P{
+    void g(){ id=new java.util.Scanner(System.in).nextInt(); }
+    void s(){ System.out.println("CD "+id); }
+}
 
-int main() {
-    AbstractProduct *p;
-    Book b; CD c;
-
-    p = &b; p->get(); p->show();
-    p = &c; p->get(); p->show();
-
-    return 0;
+public class Main{
+    public static void main(String[] a){
+        P p;
+        p=new B(); p.g(); p.s();
+        p=new C(); p.g(); p.s();
+    }
 }
